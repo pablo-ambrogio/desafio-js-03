6c0dbba957f6f81ee707a36c4bdda90ec9e34b96
pabloambrogio <pabloambrogioc@gmail.com>
2022-06-01 16:13:52 -0300

    primer commit desafios 03

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let email = document.getElementById( 'email' );
+let form = document.getElementById( 'form' );
+let text = document.getElementById( 'textError' );
+let button = document.getElementById( 'btnSend' );
+
+form.addEventListener( 'submit', ( event ) => {
+    event.preventDefault();
+} );
+
+let validarForm = () => {
+    
+    let validarEmail = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;   
+
+    if( email.value !== '' ) {
+
+        if( validarEmail.test( email.value ) ) {
+            email.classList.remove( 'form__input--error' );
+            email.classList.add( 'form__input--good' );
+        }
+        else {
+            email.classList.add( 'form__input--error' );
+            email.classList.remove( 'form__input--good' );
+            text.innerHTML = `Plase provide a valid email`;
+            text.style.visibility = 'visible';
+        }
+    } 
+    else {
+        email.classList.add( 'form__input--error' );
+        text.innerHTML = `Ingrese su email`;
+        text.style.visibility = 'visible';
+    }
+    
+}
+
+button.addEventListener( 'click', validarForm );
